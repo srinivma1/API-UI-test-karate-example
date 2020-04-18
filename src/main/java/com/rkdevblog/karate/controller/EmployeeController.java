e08db831b9a76d3431d1d547734151441abd36e6
rajithkonara <rajith.konara@tokoin.io>
2020-04-19 00:26:06 +0530

    sample controllers to test

--- src/main/java/com/rkdevblog/karate/controller/EmployeeController.java
+++ src/main/java/com/rkdevblog/karate/controller/EmployeeController.java
@@ -1,4 +1,60 @@
 package com.rkdevblog.karate.controller;
 
+import com.rkdevblog.karate.dto.AllEmployeeResponseDto;
+import com.rkdevblog.karate.dto.EmployeeResponseDto;
+import com.rkdevblog.karate.model.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/employee")
 public class EmployeeController {
+
+
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<EmployeeResponseDto> createEmployee(@RequestBody Employee employee) {
+        EmployeeResponseDto employeeResponseDto = new EmployeeResponseDto();
+        employeeResponseDto.setName(employee.getName());
+        employeeResponseDto.setMessage("Successfully created employee");
+        return ResponseEntity.ok(employeeResponseDto);
+    }
+
+    @GetMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<EmployeeResponseDto> getEmployeeById(@PathVariable int id) {
+        EmployeeResponseDto employeeResponseDto = new EmployeeResponseDto();
+        employeeResponseDto.setName("john");
+        employeeResponseDto.setMessage("Successfully returned employee");
+        if (id != 1) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(employeeResponseDto, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/all", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getAllEmployees() {
+
+        Employee employee = new Employee();
+        employee.setId("emp-1");
+        employee.setName("john");
+        employee.setAddress("winterfell");
+        employee.setEmail("john@gmail.com");
+
+        List<Employee> employees = new ArrayList<>();
+
+        for (int i = 0; i <= 5; i++) {
+            employees.add(employee);
+        }
+
+        AllEmployeeResponseDto allEmployeeResponseDto = new AllEmployeeResponseDto();
+        allEmployeeResponseDto.setEmployees(employees);
+
+        return new ResponseEntity<>(allEmployeeResponseDto, HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/rkdevblog/karate/model/Employee.java
+++ src/main/java/com/rkdevblog/karate/model/Employee.java
@@ -1,4 +1,15 @@
 package com.rkdevblog.karate.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Employee {
+
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+
 }
